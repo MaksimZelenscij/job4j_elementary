37c0b7f4fd3587a13099838b24b6887cc8e91588
Maksim Zelenscij <maksim.zelenscij@jmail.com>
2021-02-11 18:53:21 +0400

    add unit tests for SqArea class

---
+++ test/ru/job4j/condition/SqAreaTest.java
@@ -0,0 +1,36 @@
+package ru.job4j.condition;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SqAreaTest {
+
+    @Test
+    public void square() {
+        double expected = 2;
+        int p = 6;
+        int k = 2;
+        double out = SqArea.square(p, k);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void square2() {
+        double expected = 1;
+        int p = 4;
+        int k = 1;
+        double out = SqArea.square(p, k);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void square3() {
+        double expected = 3.555;
+        int p = 8;
+        int k = 2;
+        double out = SqArea.square(p, k);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+}
