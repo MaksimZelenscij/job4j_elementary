13feca5a86980678df22fb76867fcf62e19d9c6e
Maksim Zelenscij <maksim.zelenscij@jmail.com>
2021-02-12 21:01:13 +0400

    fixed Liken class

--- src/ru/job4j/condition/Liken.java
+++ src/ru/job4j/condition/Liken.java
@@ -5,13 +5,13 @@ public class Liken {
         int first = 10;
         int second = 9;
 
-        boolean result = 10 > 9;
+        boolean result = first > second;
         System.out.println(result);
 
-        result = 10 < 9;
+        result = first < second;
         System.out.println(result);
 
-        result = 10 == 9;
+        result = first == second;
         System.out.println(result);
     }
 }
